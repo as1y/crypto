cada3bd5ba51d7cb84118e8ca2ad901cfefbec52
as1y <43540128+as1y@users.noreply.github.com>
2020-12-08 12:44:23 +0300

    gg

--- config/main.php
+++ config/main.php
@@ -1,24 +1,18 @@
 <?php
 return [
     'LAYOUT' => 'MAIN',
-    'DOMAIN' => 'promocode.plus',
+    'DOMAIN' => 'dlead.ru',
 //    'UA' => 'UA-173141725-3',
     'USERTABLE' => 'users',
     'BASEMAIL' => [
-        'name' => 'ТОВАРНЫЙ CASHBACK',
-        'email' => 'promocode@promocode.plus'
+        'name' => 'DLEAD.RU',
+        'email' => 'dlead@dlead.ru'
     ],
 
     'db' => [
-        'dsn' => 'mysql:host=127.0.0.1;dbname=dbpromocode;charset=utf8;port=3306',
-        'user' => 'userpromocode',
-        'pass' => 'tSlHsoC]BG%%HM(',
-    ],
-
-    'ADMITAD' => [
-        'cliend_id' => "U5KTmLOGWL9VOmdAzvEsrtEVDOiFWu",
-        'cliend_secret' => "QUhIvPxtaXCNqsrxOkmNdhz7Dj2o4E",
-        'WID' => "1642924", //ID ПЛОЩАДКИ
+        'dsn' => 'mysql:host=127.0.0.1;dbname=dbdlead;charset=utf8;port=3306',
+        'user' => 'userdlead',
+        'pass' => '1%z}p%of%p1c1u3',
     ],
 
 
--- index.php
+++ index.php
@@ -61,17 +61,6 @@ session_start();
 $router = new Router;
 // ПУТИ ЗАДАЮТ НАДО УТОЧНИТЬ КАК РАБОТАЕТ
 $router->add( 'user/login', ['controller'=>'User', 'action'=>'index']);
-$router->add( '^category/(?P<alias>[a-z-]+)$', ['controller'=>'Category', 'action'=>'index']);
-
-//$router->add( '^promocode/(?P<alias>[a-z-]+)/?(?P<alias2>[a-z-]+)?$', ['controller'=>'Promocode', 'action'=>'index']);
-
-
-$router->add( '^shop/(?P<alias>[a-z-]+)/?(?P<alias2>[a-z-]+)?$', ['controller'=>'Shop', 'action'=>'index']);
-
-
-$router->add( '^product/(?P<alias>[0-9-]+)/?(?P<alias2>[a-z-]+)?$', ['controller'=>'Product', 'action'=>'index']);
-
-
 
 
 $router->add( '^page/(?P<action>[a-z-]+)/(?P<alias>[a-z-]+)$', ['controller'=>'Page']);
